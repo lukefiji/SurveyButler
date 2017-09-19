f20ba0573860076012d18a255f6b77766f2b4642
Luke Fiji <luke.fiji@gmail.com>
2017-09-19 00:40:59 -0700

    Set up .env and passport strategy  with Google OAuth 2.0

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // CommonJS modules on the server side - require()
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+require("dotenv").config();
+
 const app = express();
 
-// Route handler
-app.get("/", (req, res) => {
-  res.send({
-    it: "works!"
-  });
-});
+// Use passport strategies
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 // Dynamic port binding
 const PORT = process.env.PORT || 5000;
