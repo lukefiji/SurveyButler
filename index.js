c0b0869a28004c80d85ce8820d0974ef4486bd16
Luke Fiji <luke.fiji@gmail.com>
2017-09-20 23:57:18 -0700

    Add users to database only if they don't already exist

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 // CommonJS modules on the server side - require()
 const express = require('express');
 const mongoose = require('mongoose');
-require('./services/passport'); // Execute Passport config
 require('./models/User');
+require('./services/passport'); // Execute Passport config
 
 // Make a connection to MongoDB
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
 
 const app = express();
 // Apply auth routes to app by passing it in
--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+// User is model class
+const User = mongoose.model('users');
 
 // Configuring Passport strategies
 passport.use(
@@ -12,9 +16,23 @@ passport.use(
     },
     // User has come back to our server and exchanged code for profile
     (accessToken, refreshToken, profile, done) => {
-      console.log('accecss token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile:', profile);
+      // Check if user already exists
+      User.findOne({ googleId: profile.id })
+        // Database queries return promises
+        .then(existingUser => {
+          if (existingUser) {
+            // We already have a record with the given profile ID
+          } else {
+            /** 
+            * If a record doesn't exist, create
+            * new record/model instance of user.
+            * save() saves it to the database
+            */
+            new User({ googleId: profile.id }).save();
+          }
+        });
+
+      console.log(profile.id);
     }
   )
 );
