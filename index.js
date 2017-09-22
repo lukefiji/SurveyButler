bc18de6919894ea4f54d271baf99896726a5593d
Luke Fiji <luke.fiji@gmail.com>
2017-09-21 19:02:58 -0700

    Setting environment config

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 // Dynamic port binding
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT);
