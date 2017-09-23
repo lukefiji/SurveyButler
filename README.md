39d35430d99357948f555aa9bcfae5a0a1bd414c
Luke Fiji <luke.fiji@gmail.com>
2017-09-22 18:33:14 -0700

    Set up React Router /w a few routes

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # SurveyButler
 A full-stack (MERN) survey management SaaS app.
 
-The stack utilizes:
+The app's stack consists of:
 - Google OAuth
 - React + Redux
+  - React Router
   - Redux Forms
 - Node + Express
+  - Passport.js
 - SendGrid (Email Service Provider)
+- Stripe Payments
 - MongoDB
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Landing} />
+          <Route path="/surveys" component={Dashboard} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
