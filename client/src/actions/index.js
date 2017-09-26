1026c28c0f23b183b492db34645215fc85df2661
Luke Fiji <luke.fiji@gmail.com>
2017-09-25 19:45:42 -0700

    Set up Stripe API token action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,3 +7,10 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current-user');
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+// Handle Stripe payment to DB
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+  // User model is automatically updated upon successful db change
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 // Set up Stripe Checkout settings via a HOC
 class Payments extends Component {
@@ -10,7 +12,7 @@ class Payments extends Component {
         name="SurveyButler"
         description="5 email credits for $5"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_PUB_KEY}
       >
         <button className="btn">Add Credits</button>
@@ -19,4 +21,4 @@ class Payments extends Component {
   }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
