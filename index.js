6fb76dd362d121001cfc3180170bb606022058d6
Luke Fiji <luke.fiji@gmail.com>
2017-09-25 22:51:15 -0700

    Set up heroku build/deployment scripts

--- index.js
+++ index.js
@@ -33,6 +33,18 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+// If deployment evironment is production
+if (process.env.NODE_ENV === 'production') {
+  // First, check for production assets like main.js or main.css
+  app.use(express.static('client/build'));
+
+  // Second, send index.html if route isn't recognized - pass route to React
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Dynamic port binding
 const PORT = process.env.PORT;
 app.listen(PORT);
